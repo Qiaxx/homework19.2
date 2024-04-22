61c5eceb87a329b46054ea1f87cc753866cf5b6c
Qiaxx <belozertsev04@mail.ru>
2024-04-22 16:56:49 +0300

    Добавил POST-запрос

--- catalog/views.py
+++ catalog/views.py
@@ -5,4 +5,11 @@ def home(request):
     return render(request, 'home.html')
 
 def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+
+        print(f'name: {name}, phone: {phone}, message: {message}')
+
     return render(request, 'contacts.html')
