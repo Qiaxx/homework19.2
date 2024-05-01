67c96d817b3f318ac1f484c270096bd6fdbb1f89
Qiaxx <belozertsev04@mail.ru>
2024-05-01 16:58:45 +0300

    Добавлены настройки медиа, форматирование файлов, созданы модели Product, Category, связанные

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,58 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=100, verbose_name="Категория", help_text="Введите категорию"
+    )
+    description = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name="Описание категории",
+        help_text="Введите описание категории",
+    )
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=100, verbose_name="Товар", help_text="Введите название товара"
+    )
+    description = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name="Описание товара",
+        help_text="Введите описание товара",
+    )
+    image_product = models.ImageField(
+        upload_to="catalog/media",
+        blank=True,
+        null=True,
+        verbose_name="Изображение",
+        help_text="Загрузите изображение товара",
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        verbose_name="Категория",
+        help_text="Введите категорию",
+        blank=True,
+        null=True,
+        related_name='catalog',
+    )
+    cost_product = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
+
+    def __str__(self):
+        return self.name
--- catalog/views.py
+++ catalog/views.py
@@ -2,14 +2,15 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, "home.html")
+
 
 def contacts(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        phone = request.POST.get('phone')
-        message = request.POST.get('message')
+    if request.method == "POST":
+        name = request.POST.get("name")
+        phone = request.POST.get("phone")
+        message = request.POST.get("message")
 
-        print(f'name: {name}, phone: {phone}, message: {message}')
+        print(f"name: {name}, phone: {phone}, message: {message}")
 
-    return render(request, 'contacts.html')
+    return render(request, "contacts.html")
