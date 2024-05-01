88d78ee0faec9abf75bdca6ba2d75af736a899c6
Qiaxx <belozertsev04@mail.ru>
2024-05-01 17:17:52 +0300

    Добавлена директория static, произведены изменения отображения языка в settings.py, добавлена админка и отображение моделей, поиск, филтрация

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Category, Product
+
+
+@admin.register(Category)
+class CatalogAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_filter = ('name',)
+    search_fields = ('id', 'name')
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'cost_product', 'category')
+    list_filter = ('category',)
+    search_fields = ('name', 'description')
