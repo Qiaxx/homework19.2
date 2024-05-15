0410e90edfb7ab825c25c7fb3a3f716292900323
Qiaxx <belozertsev04@mail.ru>
2024-05-15 15:29:21 +0300

    Добавлена директория для перехода контроллеров на CBV, изменены названия html-файлов, изменены пути и исправлены ошибки в переходах по сайту

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,12 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts, product_details
+from catalog.views import ProductListView, ContactsView, ProductDetailsView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path("", home, name="home"),
-    path("contacts/", contacts, name="contacts"),
-    path("product/<int:pk>/", product_details, name="product_details"),
+    path("", ProductListView.as_view(), name="home"),
+    path("contacts/", ContactsView.as_view(), name="contacts"),
+    path("product/<int:pk>/", ProductDetailsView.as_view(), name="product_details"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- catalog/views.py
+++ catalog/views.py
@@ -1,26 +1,25 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, TemplateView, DetailView
 
 from catalog.models import Product
 
 
-def home(request):
-    products = Product.objects.all()
-    context = {'products': products}
-    return render(request, "product_list.html", context)
+class ProductListView(ListView):
+    model = Product
 
 
-def contacts(request):
-    if request.method == "POST":
+class ContactsView(TemplateView):
+    template_name = 'catalog/contacts.html'
+
+    def post(self, request, *args, **kwargs):
         name = request.POST.get("name")
         phone = request.POST.get("phone")
         message = request.POST.get("message")
 
         print(f"name: {name}, phone: {phone}, message: {message}")
 
-    return render(request, "contacts.html")
+        return render(request, self.template_name)
 
 
-def product_details(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    context = {'product': product}
-    return render(request, "product_details.html", context)
+class ProductDetailsView(DetailView):
+    model = Product
